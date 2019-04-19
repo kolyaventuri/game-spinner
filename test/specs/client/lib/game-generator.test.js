c39be22c759d10804cfd5e1438b9070cc8a31b7d
Kolya Venturi <kolya.venturi@enx3s.com>
2019-04-18 19:45:08 -0700

    Add game generator

---
+++ test/specs/client/lib/game-generator.test.js
@@ -0,0 +1,20 @@
+import test from 'ava';
+import proxyquire from 'proxyquire';
+
+const verbs = ['crash'];
+const nouns = ['car'];
+const adjectives = ['blue'];
+
+const {generateGame} = proxyquire('../../../../client/lib/game-generator', {
+  '../constants/verbs': verbs,
+  '../constants/nouns': nouns,
+  '../constants/adjectives': adjectives
+});
+
+test('it can generate a game string', t => {
+  const result = generateGame();
+
+  const expected = 'crash blue cars, to crash cars';
+
+  t.is(result, expected);
+});
